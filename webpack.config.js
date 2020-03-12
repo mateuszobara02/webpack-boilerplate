2dfa9d10b74907b8a8dff9f4a27c1219e8044cb4
Mateusz Obara <mateuszobara02@gmail.com>
2020-03-12 17:50:35 +0100

    Remove devServer and mode from webpack config and edit npm scripts

--- webpack.config.js
+++ webpack.config.js
@@ -3,16 +3,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    mode: 'production',
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'docs'),
         filename: 'bundle.js'
-    },
-
-    devServer: {
-        contentBase: './docs',
-        open: true
     },
 
     plugins: [
